804d70fc4218a7b284442daf7c80866479328a50
Scott Walter <scott.walter@nice.com>
2024-04-18 08:26:43 -0500

    boo

---
+++ sol-poc/src/asha/aicard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aicard',
+  templateUrl: './aicard.component.html',
+  styleUrls: ['./aicard.component.scss'],
+})
+export class AICardComponent {
+  constructor() {}
+}
