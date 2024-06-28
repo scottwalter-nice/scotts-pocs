d6f38a7f45209bb31204a25522b3c1b572e7244a
Scott Walter <scott.walter@nice.com>
2024-06-28 08:35:12 -0500

    wip

---
+++ sol-poc/src/cards/cool-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cool-card',
+  templateUrl: './cool-card.component.html',
+  styleUrls: ['./cool-card.component.scss'],
+})
+export class CoolCardComponent {
+  constructor() {}
+}
